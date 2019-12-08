008b9dda58da2c158140a51a88e0bf76d33bd46d
herreio <git@herre.io>
2019-12-08 21:16:24 +0100

    data-raw/ updated

--- data-raw/corpus.R
+++ data-raw/corpus.R
@@ -1,11 +1,57 @@
-devtools::load_all()
-pc_titles <- get_pc_titles()
-titles_eng <- readLines(file.path(pc_titles$all_lang, "eng.txt"))
-titles_eng <- tm::VCorpus(tm::VectorSource(eng))
+# devtools::load_all()
+
+pc_titles <- maxplanckr::get_pc_titles()
+
+# /////////////////////// #
+# /// LANGUAGE SCHEME /// #
+# /////////////////////// #
+
+titles_eng <- maxplanckr::lines_to_corpus(file.path(pc_titles$all_lang, "eng.txt"))
 usethis::use_data(titles_eng, overwrite=T)
-titles_cat <- plain_to_corpus(pc_titles$cat_lang)
+titles_eng_raw <- maxplanckr::lines_to_corpus(file.path(pc_titles$all_lang, "eng_raw.txt"))
+usethis::use_data(titles_eng_raw, overwrite=T)
+titles_eng_year <- maxplanckr::plain_to_corpus(pc_titles$all_lang_year)
+usethis::use_data(titles_eng_year, overwrite=T)
+titles_eng_genre <- maxplanckr::plain_to_corpus(pc_titles$all_lang_genre)
+usethis::use_data(titles_eng_genre, overwrite=T)
+titles_eng_year_genre <- maxplanckr::plain_to_corpus(pc_titles$all_lang_year_genre)
+usethis::use_data(titles_eng_year_genre, overwrite=T)
+
+# //////////////////// #
+# /// INDEX SCHEME /// #
+# //////////////////// #
+
+titles_cat <- maxplanckr::plain_to_corpus(pc_titles$cat_lang)
 usethis::use_data(titles_cat, overwrite=T)
-titles_mpi <- plain_to_corpus(pc_titles$mpi_lang)
+titles_cat_year <- maxplanckr::plain_to_corpus(pc_titles$cat_lang_year)
+usethis::use_data(titles_cat_year, overwrite=T)
+titles_cat_genre <- maxplanckr::plain_to_corpus(pc_titles$cat_lang_genre)
+usethis::use_data(titles_cat_genre, overwrite=T)
+titles_cat_year_genre <- maxplanckr::plain_to_corpus(pc_titles$cat_lang_year_genre)
+usethis::use_data(titles_cat_year_genre, overwrite=T)
+
+# //////////////////////// #
+# /// INSTITUTE SCHEME /// #
+# //////////////////////// #
+
+titles_mpi <- maxplanckr::plain_to_corpus(pc_titles$mpi_lang)
 usethis::use_data(titles_mpi, overwrite=T)
-titles_pers <- plain_to_corpus(pc_titles$pers_lang)
+titles_mpi_genre <- maxplanckr::plain_to_corpus(pc_titles$mpi_lang_genre)
+usethis::use_data(titles_mpi_genre, overwrite=T)
+titles_mpi_year <- maxplanckr::plain_to_corpus(pc_titles$mpi_lang_year)
+usethis::use_data(titles_mpi_year, overwrite=T)
+titles_mpi_year_genre <- maxplanckr::plain_to_corpus(pc_titles$mpi_lang_year_genre)
+usethis::use_data(titles_mpi_year_genre, overwrite=T)
+
+# ///////////////////// #
+# /// PERSON SCHEME /// #
+# ///////////////////// #
+
+titles_pers <- maxplanckr::plain_to_corpus(pc_titles$pers_lang)
 usethis::use_data(titles_pers, overwrite=T)
+titles_pers_genre <- maxplanckr::plain_to_corpus(pc_titles$pers_lang_genre)
+usethis::use_data(titles_pers_genre, overwrite=T)
+titles_pers_year <- maxplanckr::plain_to_corpus(pc_titles$pers_lang_year)
+usethis::use_data(titles_pers_year, overwrite=T)
+titles_pers_year_genre <- maxplanckr::plain_to_corpus(pc_titles$pers_lang_year_genre)
+usethis::use_data(titles_pers_year_genre, overwrite=T)
--- data-raw/internal.R
+++ data-raw/internal.R
@@ -1,25 +1,25 @@
 # devtools::load_all()
 
-pure_ctx <- read_data(pc_graph$pure_nodes_ctx)
-pure_ous <- read_data(pc_graph$pure_nodes_ous)
-pure_ctx_ous <- read_data(pc_graph$pure_edges_ctx_ous)
+pure_ctx <- maxplanckr::read_data(pc_graph$pure_nodes_ctx)
+pure_ous <- maxplanckr::read_data(pc_graph$pure_nodes_ous)
+pure_ctx_ous <- maxplanckr::read_data(pc_graph$pure_edges_ctx_ous)
 
-mpis_ous <- read_data(pc_graph$mpis_nodes_ous)
-mpis_ctx <- read_data(pc_graph$mpis_nodes_ctx)
-mpis_cats <- read_data(pc_graph$mpis_nodes_cats)
-mpis_tags <- read_data(pc_graph$mpis_nodes_tags)
-mpis_ous_tree <- read_data(pc_graph$mpis_nodes_ous_tree_full)
-mpis_ous_kids <- read_data(pc_graph$mpis_nodes_ous_tree_children)
+mpis_ous <- maxplanckr::read_data(pc_graph$mpis_nodes_ous)
+mpis_ctx <- maxplanckr::read_data(pc_graph$mpis_nodes_ctx)
+mpis_cats <- maxplanckr::read_data(pc_graph$mpis_nodes_cats)
+mpis_tags <- maxplanckr::read_data(pc_graph$mpis_nodes_tags)
+mpis_ous_tree <- maxplanckr::read_data(pc_graph$mpis_nodes_ous_tree_full)
+mpis_ous_kids <- maxplanckr::read_data(pc_graph$mpis_nodes_ous_tree_children)
 
-mpis_ctx_edges <- read_data(pc_graph$mpis_edges_ous_ctx)
-mpis_ous_edges <- read_data(pc_graph$mpis_edges_ous_ous)
-mpis_cats_edges <- read_data(pc_graph$mpis_edges_ous_cats)
-mpis_tags_edges <- read_data(pc_graph$mpis_edges_ous_tags)
-mpis_cats_tags_edges <- read_data(pc_graph$mpis_edges_cats_tags)
+mpis_ctx_edges <- maxplanckr::read_data(pc_graph$mpis_edges_ous_ctx)
+mpis_ous_edges <- maxplanckr::read_data(pc_graph$mpis_edges_ous_ous)
+mpis_cats_edges <- maxplanckr::read_data(pc_graph$mpis_edges_ous_cats)
+mpis_tags_edges <- maxplanckr::read_data(pc_graph$mpis_edges_ous_tags)
+mpis_cats_tags_edges <- maxplanckr::read_data(pc_graph$mpis_edges_cats_tags)
 
-sel_ctx <- read_data(pc_graph$sel_nodes_ctx)
-sel_ous <- read_data(pc_graph$sel_nodes_ous)
-sel_ous_ctx <- read_data(pc_graph$sel_edges_ous_ctx)
+sel_ctx <- maxplanckr::read_data(pc_graph$sel_nodes_ctx)
+sel_ous <- maxplanckr::read_data(pc_graph$sel_nodes_ous)
+sel_ous_ctx <- maxplanckr::read_data(pc_graph$sel_edges_ous_ctx)
 
 usethis::use_data(pure_ctx, pure_ous, pure_ctx_ous,
                   mpis_ous, mpis_ctx, mpis_tags, mpis_cats,
