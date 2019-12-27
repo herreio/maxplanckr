5900b9f7a2d84e5b391475968fabfcfeb07580d7
herreio <git@herre.io>
2019-12-27 18:01:35 +0100

    R/ python updated

--- R/pyimp.R
+++ R/pyimp.R
@@ -1,8 +1,21 @@
+#' Use python environment
+#'
+#' @export
+setpython <- function() {
+  reticulate::use_virtualenv(
+    system.file("python/env",
+      package = "maxplanckr"),
+    required = T)
+}
+
 #' Prepocess character vector
 #'
 #' @export
 preprocess <- function(title) {
-  reticulate::use_virtualenv(system.file("python/env", package = "maxplanckr"), required = T)
-  extract <- reticulate::import_from_path("extract", path = system.file("python", package = "maxplanckr"), convert = T)
+  setpython()
+  extract <- reticulate::import_from_path(
+    "extract",
+    path = system.file("python", package = "maxplanckr"),
+    convert = T)
   extract$titles$preprocess$clean(title)
 }
